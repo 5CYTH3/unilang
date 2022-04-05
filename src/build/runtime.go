bdaf4d7134f4b2a8223ac4e54af0391331a1a6f2
5CYTH3 <ScytheD@outlook.fr>
2022-04-05 09:43:04 +0200

    Trying to compile and show output (using assembly language).

--- src/build/runtime.go
+++ src/build/runtime.go
@@ -13,40 +13,78 @@ import (
 func GenerateAssembly(entry []t.Tokens) {
 	f, _ := os.Create("out.asm")
 	f.WriteString(`segment .text
-	global _start
+dump:
+	mov r9, rax
+	sub rsp, 40
+	mov BYTE [rsp+31], 10
+	lea rcx, [rsp+30]
 
-_start:` + "\n")
+.L2:
+	mov rax, rdi
+	lea r8, [rsp+32]
+	mul r9
+	mov rax, rdi
+	sub r8, rcx
+	shr rdx, 3
+	lea rsi, [rdx+rdx*4]
+	add rsi, rsi
+	sub rax, rsi
+	add eax, 48
+	mov BYTE [rcx], al
+	mov rax, rdi
+	mov rdi, rdx
+	mov rdx, rcx
+	sub rcx, 1
+	cmp rax, 9
+	ja  .L2
+	lea rax, [rsp+32]
+	mov edi, 1
+	sub rdx, rax
+	xor eax, eax
+	lea rsi, [rsp+32+rdx]
+	mov rdx, r8
+	mov rax, 1
+	syscall
+	add rsp, 40
+	ret
+
+global _start` + "\n")
+	f.WriteString("_start:\n")
 	for _, i := range entry {
 		if i.GetOp() == t.OP_PUSH {
 			f.WriteString(fmt.Sprintf("	;; -- pushing value %d --\n", i.GetValue()))
-			f.WriteString(fmt.Sprintf("mov rax, %d\n", i.GetValue()))
+			f.WriteString(fmt.Sprintf("	mov rax, %d\n", i.GetValue()))
 		} else if i.GetOp() == t.OP_PLUS {
 			f.WriteString(`	;; -- adding 2 values --
+	pop rax
 	pop rbx
 	add rax, rbx
-	ret`)
-		} else if i.GetOp() == t.OP_DUMP {
-			// Asm
+	push rax
+	ret` + "\n")
+		} else if i.GetOp() == t.OP_DIV {
+			f.WriteString(`;; -- Divison not supported for now --`)
 		} else if i.GetOp() == t.OP_MIN {
 			f.WriteString(`	;; -- substracting 2 values --
-	sub rax, rbx`)
+	pop rax
+	pop rbx
+	sub rax, rbx
+	push rax
+	`)
 		} else if i.GetOp() == t.OP_MUL {
 			f.WriteString(` ;; -- multiplication is not supported --`)
-		} else if i.GetOp() == t.OP_DIV {
-			f.WriteString(` ;; -- division is not supported --`)
+		} else if i.GetOp() == t.OP_DUMP {
+			f.WriteString(`	pop rdi
+	call dump` + "\n")
 		}
 	}
-	f.WriteString(`jmp @main_exit
-`)
-	f.WriteString(`; -- file end --
-@main_exit:
-	pop rax
-	ret`)
+	f.WriteString(`	mov rax, 60
+	mov rdi, 0
+	syscall`)
 	f.Close()
 	ExecuteCommand("nasm", "-f", "elf64", "out.asm")
-	ExecuteCommand("ld", "-o", "out", "out.o")
-	os.Remove("out.asm")
-	os.Remove("out.o")
+	ExecuteCommand("ld", "-o", "out.exe", "out.o")
+	defer os.Remove("out.asm")
+	defer os.Remove("out.o")
 }
 
 func ExecuteCommand(cmdName string, cmdArg ...string) ([]byte, error) {
